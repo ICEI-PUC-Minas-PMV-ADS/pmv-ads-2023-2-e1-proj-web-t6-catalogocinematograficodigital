674d15f3ee2aa68117912aea0ef98630d1cdae6f
Fabio R. Barros <142315849+FabioBarrosPUC@users.noreply.github.com>
2023-10-09 07:49:52 -0300

    Update cadastro.js
    atualização do formulário, responsividade para o menu a partir de 717px

--- codigo-fonte/scr/assets/js/cadastro.js
+++ codigo-fonte/scr/assets/js/cadastro.js
@@ -78,77 +78,83 @@ closeMenu.addEventListener('click', () => {
     }, 200);
 })
 
-// Capturando o elemento de formulário pelo seu ID "cadastroForm"
-const form = document.getElementById("cadastroForm");
 
-// Adicionando um evento de "submit" ao formulário
-form.addEventListener("submit", function (event) {
-    // Impedindo o comportamento padrão de envio do formulário
-    event.preventDefault();
+// Aguarde o carregamento completo da página antes de executar o código
+document.addEventListener("DOMContentLoaded", function () {
+    // Capturando o elemento do botão de envio pelo seu ID "submitButton"
+    const submitButton = document.getElementById("submitButton");
 
-    // Capturando os valores dos campos de entrada
-    const nome = document.getElementById("nome").value;
-    const apelido = document.getElementById("apelido").value;
-    const email = document.getElementById("email").value;
-    const senha = document.getElementById("senha").value;
-    const confirmarSenha = document.getElementById("confirmarSenha").value;
+    // Adicionando um evento de clique ao botão de envio
+    submitButton.addEventListener("click", function (event) {
+        // Impedindo o comportamento padrão de envio do formulário
+        event.preventDefault();
 
-    // Validar nome
-    const nomeRegex = /^[A-Za-z\s]+$/;
-    // Verificando se o nome não corresponde ao padrão definido na expressão regular
-    if (!nome.match(nomeRegex)) {
-        // Exibindo uma mensagem de erro abaixo do campo "nome"
-        document.getElementById("nomeError").textContent = "Nome inválido";
-        return; // Saindo da função para impedir o envio do formulário
-    }
-    // Limpando a mensagem de erro se o nome for válido
-    document.getElementById("nomeError").textContent = "";
+        // Capturando os valores dos campos de entrada
+        const nome = document.getElementById("nome").value;
+        const apelido = document.getElementById("apelido").value;
+        const email = document.getElementById("email").value;
+        const senha = document.getElementById("senha").value;
+        const confirmarSenha = document.getElementById("confirmarSenha").value;
 
-    // Validar apelido
-    // Verificando se o apelido é apenas espaços em branco
-    if (apelido.trim() === "") {
-        document.getElementById("apelidoError").textContent = "Apelido inválido";
-        return;
-    }
-    document.getElementById("apelidoError").textContent = "";
+        // Validar nome
+        const nomeRegex = /^[A-Za-z\s]+$/;
+        // Verificando se o nome não corresponde ao padrão definido na expressão regular
+        if (!nome.match(nomeRegex)) {
+            // Exibindo uma mensagem de erro abaixo do campo "nome"
+            document.getElementById("nomeError").textContent = "Nome inválido";
+            return; // Saindo da função para impedir o envio do formulário
+        }
+        // Limpando a mensagem de erro se o nome for válido
+        document.getElementById("nomeError").textContent = "";
 
-    // Validar email
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    // Verificando se o email não corresponde ao padrão definido na expressão regular
-    if (!email.match(emailRegex)) {
-        document.getElementById("emailError").textContent = "E-mail inválido";
-        return;
-    }
-    document.getElementById("emailError").textContent = "";
+        // Validar apelido
+        // Verificando se o apelido é apenas espaços em branco
+        if (apelido.trim() === "") {
+            document.getElementById("apelidoError").textContent = "Apelido inválido";
+            return;
+        }
+        document.getElementById("apelidoError").textContent = "";
 
-    // Validar senha
-    const senhaRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$!%^&*])[A-Za-z\d@#$!%^&*]{5,}$/;
-    // Verificando se a senha não corresponde ao padrão definido na expressão regular
-    if (!senha.match(senhaRegex)) {
-        document.getElementById("senhaError").textContent = "Senha inválida";
-        return;
-    }
-    document.getElementById("senhaError").textContent = "";
+        // Validar email
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        // Verificando se o email não corresponde ao padrão definido na expressão regular
+        if (!email.match(emailRegex)) {
+            document.getElementById("emailError").textContent = "E-mail inválido";
+            return;
+        }
+        document.getElementById("emailError").textContent = "";
 
-    // Confirmar senha
-    // Verificando se a senha e a confirmação de senha são iguais
-    if (senha !== confirmarSenha) {
-        document.getElementById("confirmarSenhaError").textContent = "Senhas não coincidem";
-        return;
-    }
-    document.getElementById("confirmarSenhaError").textContent = "";
+        // Validar senha
+        const senhaRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$!%^&*])[A-Za-z\d@#$!%^&*]{5,}$/;
+        // Verificando se a senha não corresponde ao padrão definido na expressão regular
+        if (!senha.match(senhaRegex)) {
+            document.getElementById("senhaError").textContent = "Senha inválida";
+            return;
+        }
+        document.getElementById("senhaError").textContent = "";
 
-    // Armazenar os dados no Web Storage
-    const usuario = {
-        nome,
-        apelido,
-        email,
-        senha
-    };
-    // Convertendo o objeto "usuario" em uma string JSON e armazenando no Web Storage com a chave "usuario"
-    localStorage.setItem("usuario", JSON.stringify(usuario));
-    // Exibindo um alerta de sucesso
-    alert("Cadastro realizado com sucesso!");
-    // Resetando o formulário para limpar os campos
-    form.reset();
+        // Confirmar senha
+        // Verificando se a senha e a confirmação de senha são iguais
+        if (senha !== confirmarSenha) {
+            document.getElementById("confirmarSenhaError").textContent = "Senhas não coincidem";
+            return;
+        }
+        document.getElementById("confirmarSenhaError").textContent = "";
+
+        // Armazenar os dados no Web Storage
+        const usuario = {
+            nome,
+            apelido,
+            email,
+            senha
+        };
+        // Convertendo o objeto "usuario" em uma string JSON e armazenando no Web Storage com a chave "usuario"
+        localStorage.setItem("usuario", JSON.stringify(usuario));
+
+        // Exibindo um alerta de sucesso
+        alert("Cadastro realizado com sucesso!");
+
+        // Resetando o formulário para limpar os campos
+        document.getElementById("cadastroForm").reset();
+    });
 });
